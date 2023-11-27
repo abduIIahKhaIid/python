5ddc6e02e06f9b1c49099448574b650c81bcdaa0
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-11-27 23:08:10 +0500

    first commit

--- String/first.py
+++ String/first.py
@@ -11,8 +11,15 @@ first  = f""" This is a multi line
 Comment can't "How are you" {name}
 'How are you'
 """
+print(first)
+
+# using blackslash
 name = 'Abdullah Khalid \' CS \' Student'
 print(name)
+print("name "+ \
+      " How are you "\
+        "Nothing can do it again")
 
-print(first)
+
+
 
